e814eb7e4351f98c1600ee69617d92652d89f707
Tristan Kernick <tristan.kernick@gmail.com>
2020-05-04 22:24:57 -0500

    Logs are now stored in logs directory.  3 days then recycled.

--- helpers/logger.py
+++ helpers/logger.py
@@ -1,14 +1,14 @@
 import logging
-
+from logging import handlers
+from helpers import re
 
 def get_logger(name: str = 'general') -> logging.Logger:
     l = logging.getLogger(name)
-    l.setLevel(logging.DEBUG)
-    h = logging.FileHandler('nparse.log')
+    l.setLevel(logging.INFO)
+    h = logging.handlers.TimedRotatingFileHandler(
+        './logs/nparse.log', when='d', interval=1, backupCount=3
+    )
     f = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     h.setFormatter(f)
     l.addHandler(h)
     return l
-
-
-
